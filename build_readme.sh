562a7bc619dbc3a95e6b0ebbd6983e18fdfeb4bd
Gabriel Geisler Mesevage <gabriel.mesevage@gmail.com>
2020-03-27 22:16:56 +0200

    fixed enormous size of equations

--- build_readme.sh
+++ build_readme.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pandoc README.tex.md --webtex='https://latex.codecogs.com/png.latex?%5Cdpi{300}' -t html | pandoc -f html -o README.md
+pandoc README.tex.md --webtex='https://latex.codecogs.com/png.latex?' -t html | pandoc -f html -o README.md
